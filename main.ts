96356b078ac9accb940f385d0a6c2c0d4eda32b4
Daniel-Brushett <dj.brushett@stu.ocsb.ca>
2026-02-04 13:22:08 -0500

    added a new date, added more information between {/* and */}

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Daniel John Brushett
+ * Created on: 02/03/26
+ * This program makes the microbit state 'Hello, World!'
 */
 
 basic.showString('Hello, World!')
