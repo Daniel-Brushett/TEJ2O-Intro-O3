b2fbcb9c9315b23fbe281aa5939850d93fd0454b
Daniel-Brushett <dj.brushett@stu.ocsb.ca>
2026-02-24 13:14:15 -0500

    Change and comitted

--- main.ts
+++ main.ts
@@ -2,8 +2,26 @@
  *
  * Created by: Daniel John Brushett
  * Created on: 02/03/26
- * First Update happened on: 02/11/26
  * This program makes the microbit state 'Hello, Daniel!'
 */
 
-basic.showString('Hello, Daniel')
+let currentTemperatureCelcius: number
+let currentTemperatureKelvin: number
+
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+//variables
+let lightvalue: number 
+let lightValuePercentage: number 
+
+input.onButtonPressed(Button.A, function ()  {
+ // process
+lightvalue = input.lightLevel()
+lightValuePercentage = (lightvalue / 255) * 100
+lightValuePercentage = Math.round(lightValuePercentage)
+
+// output
+basic.clearScreen()
+basic.showString('Percentage light is: ' + lightValuePercentage.toString() + '%.')
+})
