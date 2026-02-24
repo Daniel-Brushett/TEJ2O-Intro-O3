7f6c7266255fe4c266b250ea3b7195fde1e855e6
Daniel-Brushett <dj.brushett@stu.ocsb.ca>
2026-02-24 13:26:58 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@
  *
  * Created by: Daniel John Brushett
  * Created on: 02/03/26
- * This program makes the microbit state 'Hello, Daniel!'
+ * This program makes the microbit state temperature
 */
 
 let currentTemperatureCelcius: number
